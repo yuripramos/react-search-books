549b6f004d182d5036e8313aa5890a0e32703ab5
Yuri Ramos <yuri.pramos@gmail.com>
2017-03-08 23:50:43 -0300

    removing coments from header component

--- js/layout/header.js
+++ js/layout/header.js
@@ -3,33 +3,6 @@ import AutoComplete from '../../node_modules/material-ui/AutoComplete';
 import ReactDOM from 'react-dom';
 
 
-//  const Header = React.createClass({
-
-//   handleSubmit : function (e) {
-//     e.preventDefault();
-//     var input = ReactDOM.findDOMNode(this.refs.search);
-
-//     this.props.localSubmit(input.value);
-
-//     input.value = '';
-
-//   },
-//   render : function () {
-//     return (
-//       <header className="codrops-header">
-// 				<div>
-// 					<form className="form-inline" style={{marginTop: 30 + 'px'}} onSubmit={this.handleSubmit}>
-//             <div className="form-group">
-// 						    <input className="form-control" placeholder="Book Title ..." ref="search"/>
-// 						    <button className="btn btn-default"><span className="glyphicon glyphicon-search"></span></button>
-//             </div>
-// 					</form>
-// 				</div>
-// 			</header>
-//     );
-//   }
-// });
-
 const Header = React.createClass ({
   getInitialState : function () {
     return ({dataSource:[]});
